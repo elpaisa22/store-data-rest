d97a1715c1b04e648e45285f6a58e071750fc48e
elpaisa22 <sebastianecclesia@gmail.com>
2017-04-04 18:11:01 -0300

    Se agrega informacion por defecto en el producto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,6 +8,6 @@ insert into categoria(id, descripcion) values (1, 'Hombre')
 insert into categoria(id, descripcion) values (2, 'Mujer')
 
 -- Productos
-insert into producto(descripcion, precio, codigo, stock, categoria_id) values ('Remera Nike', 30.50, '2132154121', 20, 1)
+insert into producto(descripcion, precio, codigo, stock, categoria_id, detalles, pie) values ('Remera Nike', 30.50, '2132154121', 20, 1, 'Con la impronta deportiva de la marca y lista para acompañarte en tus actividades diarias con total comodidad, presentamos la Remera Nike New Jdi Swoosh.', 'Nombre:  Remera Nike New Jdi Swoosh Género:  Masculino Adecuado para: Uso Diario Estilo de la pieza: Estampado Cuello: Redondo Material Principal: AlgodónManga: CortaComposición: algodón.Origen:  Argentina')
 insert into producto(descripcion, precio, codigo, stock, categoria_id) values ('Buzo Adidas', 60.25, '5485454', 5, 2)
 insert into producto(descripcion, precio, codigo, stock, categoria_id) values ('Pantalon Puma', 101.66, '5454', 10, 1)
